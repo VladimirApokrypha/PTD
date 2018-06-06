a196d3e978f80c165d1d478ac1f8473107ee44d1
EkspulDew <37208934+EkspulDew@users.noreply.github.com>
2018-06-06 18:39:55 +0700

    Add files via upload

---
+++ DomainPTD/DomainClasses/CASCombine.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DomainPTD.DomainInterfaces;
+
+namespace DomainPTD.DomainClasses
+{
+    class CASCombine : IWorkFile
+    {
+        public string Path
+        {
+            get
+            {
+                return _path;
+            }
+            set
+            {
+                _path = value;
+                Extension = _path.Split(new Char[] { '.' }).LastOrDefault();
+            }
+        }
+        string _path;
+        public string Extension { get; set; }
+        public IWorker worker { get; set; }
+    }
+}
